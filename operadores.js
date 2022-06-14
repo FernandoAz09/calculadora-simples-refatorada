edc85c9f346f09f27afd21e62683af0f08069719
Fernando <fsazevedo09@gmail.com>
2022-06-14 17:02:03 -0300

    Created class Calculator + started method of All Clear

--- operadores.js
+++ operadores.js
@@ -1,1 +1,26 @@
-const numberButton = querySelectorAll([data-number])
+const numberButton = querySelectorAll('[data-number]')
+const operationButton = querySelectorAll('[data-operator]')
+const equalsButton = querySelector('[data-equals]')
+const deleteButton = querySelector('[data-delete]')
+const allClearButton = querySelector('[data-all-clear]')
+
+const previousOperandTextElement = querySelector('[data-previous-operand]')
+const currentOperandTextElement = querySelector('[data-current-operand]')
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+}
+
+const calculator = new Calculator(
+    previousOperandTextElement,
+    currentOperandTextElement
+)
